e88e8cfe90bb2de9eadac85f9761b39b4364faa8
Barış DEMİRCİ <demirci.baris38@gmail.com>
2020-12-25 16:40:30 +0300

    🎭 ICONS.random() (random icon)

--- src/index.ts
+++ src/index.ts
@@ -1,46 +1,51 @@
 import { Image, registerFont } from "canvas";
 import { Canvas, resolveImage } from "canvas-constructor";
 import { ICONS } from "./icons";
+import { resolve } from "path";
 
-registerFont(__dirname + "/../../src/assets/font.ttf", {
-    family: "Minecraft",
+registerFont(resolve(__dirname, "..", "assets", "font.ttf"), {
+	family: "Minecraft",
 });
 
 class AchievementCreator {
-    private static async createCanvas(): Promise<Canvas> {
-        const background = await resolveImage(__dirname + "/../../src/assets/bg.png");
-        const canvas = new Canvas(503, 100).printImage(background, 0, 0);
-        return canvas;
-    }
-    private static async fetchImage(name: string): Promise<Image> {
-        const image = await resolveImage(`${__dirname}/../../src/assets/icons/${name}.png`);
-        return image;
-    }
-    static async create(
-        icon: string,
-        title: string,
-        content: string,
-    ): Promise<Buffer> {
-        if (!Object.values(ICONS).includes(icon))
-            throw new Error(
-                `${icon} not found in icons folder. See "ICONS" export for more information.`,
-            );
-        if (title.length > 28)
-            throw new Error("Title can not be longer than 28 characters");
-        if (content.length > 28)
-            throw new Error("Content can not be longer than 28 characters");
-        const image = await this.fetchImage(icon);
-        const buffer = (await this.createCanvas())
-            .setTextFont("24px Minecraft")
-            .setTextAlign("left")
-            .setColor("#f8f628")
-            .printText(title, 125, 40)
-            .setColor("#ffffff")
-            .printText(content, 125, 80)
-            .printImage(image, 35, 20)
-            .toBuffer();
-        return buffer;
-    }
+	private static async _createCanvas(): Promise<Canvas> {
+		const background = await resolveImage(
+			resolve(__dirname, "..", "assets"),
+		);
+		const canvas = new Canvas(503, 100).printImage(background, 0, 0);
+		return canvas;
+	}
+	private static async _fetchImage(name: string): Promise<Image> {
+		const image = await resolveImage(
+			resolve(__dirname, "..", "assets", "icons", `${name}.png`),
+		);
+		return image;
+	}
+	static async create(
+		icon: string,
+		title: string,
+		content: string,
+	): Promise<Buffer> {
+		if (!Object.values(ICONS).includes(icon))
+			throw new Error(
+				`${icon} not found in icons folder. See "ICONS" export for more information.`,
+			);
+		if (title.length > 28)
+			throw new Error("Title can not be longer than 28 characters");
+		if (content.length > 28)
+			throw new Error("Content can not be longer than 28 characters");
+		const image = await this._fetchImage(icon);
+		const buffer = (await this._createCanvas())
+			.setTextFont("24px Minecraft")
+			.setTextAlign("left")
+			.setColor("#f8f628")
+			.printText(title, 125, 40)
+			.setColor("#ffffff")
+			.printText(content, 125, 80)
+			.printImage(image, 35, 20)
+			.toBuffer();
+		return buffer;
+	}
 }
 
 export { ICONS, AchievementCreator };
--- tests/example.test.ts
+++
@@ -1,9 +0,0 @@
-/*
-    tests coming soon
-*/
-import { expect } from "chai";
-describe("Example Test", () => {
-    it("Should pass", () => {
-        expect(true).to.be.true;
-    });
-});
